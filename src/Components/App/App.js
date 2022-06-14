e3a4e959d9bc55097716e5c9ad6932052d21338d
Carlos Correa <76818683+Meatperson@users.noreply.github.com>
2022-06-14 18:49:11 -0400

    User's can save playlists

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -35,6 +35,7 @@ export class App extends React.Component {
     this.addTrack = this.addTrack.bind(this)
     this.removeTrack = this.removeTrack.bind(this)
     this.updatePlaylistName = this.updatePlaylistName.bind(this)
+    this.savePlaylist = this.savePlaylist.bind(this)
   };
 
   addTrack(track) {
@@ -58,6 +59,9 @@ export class App extends React.Component {
     this.setState({ playlistName: name })
   }
 
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+  }
 
   render() {
     return (
@@ -80,6 +84,7 @@ export class App extends React.Component {
                   playlistTracks={this.state.playlistTracks}
                   onRemove={this.removeTrack}
                   onNameChange={this.updatePlaylistName}
+                  onSave={this.savePlaylist}
                   />}
               </div>
             </div>
