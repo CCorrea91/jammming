b6a6027bf33b512a1ae6026a3d5ebaacc51c2485
Carlos Correa <76818683+Meatperson@users.noreply.github.com>
2022-06-14 20:25:55 -0400

    Added Spotify.js

---
+++ src/Components/util/Spotify.js
@@ -0,0 +1,14 @@
+let accessToken;
+
+class Spotify extends React.Component {
+
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken
+        } else {
+            
+        }
+    }
+};
+
+export default Spotify;
